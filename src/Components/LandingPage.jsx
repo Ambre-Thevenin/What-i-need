c4812faa7e0914c9cf446e2b18a9d532719c9f7c
Matthieu Lemarchand <lemarchand.matth@gmail.com>
2021-10-28 09:38:51 +0200

    Set Landing Page

---
+++ src/Components/LandingPage.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from './LandingPage.module.css';
+
+function LandingPage() {
+  return (
+    <body className={styles.body}>
+      <header className={styles.header}> Mettre ici le Logo, le burger et le +</header>
+      <main className={styles.main}>
+        <div className={styles.surprise}>Surprise</div>
+        <div className={styles.welcome}>Mettre ici le Bonjour Alex, nous sommes le date XXX, il est heure XXX</div>
+        <div className={styles.input}>champs de saisie (input)</div>
+
+        <div className={styles.cards}>
+          <img src="https://www.radars-auto.com/carte-radar/carte-france-index.png" alt="cardMap" className={styles.card} />
+
+          <img src="https://www.radars-auto.com/carte-radar/carte-france-index.png" alt="cardMeteo" className={styles.card} />
+
+          <img src="https://www.radars-auto.com/carte-radar/carte-france-index.png" alt="cardShops" className={styles.card} />
+
+          <img src="https://www.radars-auto.com/carte-radar/carte-france-index.png" alt="cardAstro" className={styles.card} />
+        </div>
+      </main>
+    </body>
+  );
+}
+
+export default LandingPage;
