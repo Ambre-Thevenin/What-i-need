16da3272251cfc60eca4e58798fdad970694268e
Matthieu Lemarchand <lemarchand.matth@gmail.com>
2021-10-29 10:48:40 +0200

    actualisation import date sur LandingPage

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './LandingPage.module.css';
-import CurrentDate from '../components/Date';
+import CurrentDate from './components/Date';
 
 function LandingPage() {
   return (
