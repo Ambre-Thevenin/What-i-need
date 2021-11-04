5ba3a619a05358e105d9d91cad09697e8a792ab1
Mel <melissa.olas@gmail.com>
2021-11-04 15:29:41 +0100

    updating app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
-// import LandingPage from './Components/LandingPage';
+import LandingPage from './Components/LandingPage';
 import './App.css';
-import ContactForm from './components/contactForm';
-import WinSquad from './components/squadPictures';
-import Header from './components/Header';
 
 function App() {
   return (
     <div>
-      <Header />
-      <WinSquad />
-      <ContactForm />
+      <LandingPage />{' '}
     </div>
   );
 }
