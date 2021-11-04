b8d7473ca05e7f7b15ef123391bdf00ac40a3e1b
Matthieu Lemarchand <lemarchand.matth@gmail.com>
2021-11-04 16:10:00 +0100

    Travail de la semaine Sprint 2

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,29 +1,36 @@
 import React from 'react';
 import styles from './LandingPage.module.css';
 import CurrentDate from './Date';
+import StartAstroPage from './ModuleAstro';
+import StartMeteoPage from './ModuleMeteo';
+import StartShopsPage from './ModuleShops';
+import StartSNCFPage from './ModuleSNCF';
+import AdressSearch from './AdressSearch';
 
 function LandingPage() {
   return (
-    <body className={styles.body}>
+    <main className={styles.main}>
       <header className={styles.header}> Mettre ici le Logo, le burger et le +</header>
-      <main className={styles.main}>
-        <div className={styles.surprise}>Surprise</div>
-        <div className={styles.welcome}>
-          <CurrentDate />
-        </div>
-        <div className={styles.input}>champs de saisie (input)</div>
 
-        <div className={styles.cards}>
-          <img src="https://www.radars-auto.com/carte-radar/carte-france-index.png" alt="cardMap" className={styles.card} />
+      <div className={styles.surprise}>Surprise</div>
+      <div className={styles.welcome}>
+        <CurrentDate />
+      </div>
+      <div className={styles.input}>
+        {' '}
+        <AdressSearch />{' '}
+      </div>
 
-          <img src="https://www.radars-auto.com/carte-radar/carte-france-index.png" alt="cardMeteo" className={styles.card} />
+      <div className={styles.cards}>
+        <StartSNCFPage />
 
-          <img src="https://www.radars-auto.com/carte-radar/carte-france-index.png" alt="cardShops" className={styles.card} />
+        <StartMeteoPage />
 
-          <img src="https://www.radars-auto.com/carte-radar/carte-france-index.png" alt="cardAstro" className={styles.card} />
-        </div>
-      </main>
-    </body>
+        <StartShopsPage />
+
+        <StartAstroPage />
+      </div>
+    </main>
   );
 }
 
