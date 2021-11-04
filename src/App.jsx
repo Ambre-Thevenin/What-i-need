85bdcb649dc6a6ee7b9d6b62cadc9b48353fc93b
Ambre-Thevenin <ambre.thvn@gmail.com>
2021-11-04 14:50:56 +0100

    Updated App.jsx with Route creation - wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import LandingPage from './Components/LandingPage';
 import './App.css';
+import ContactForm from './Components/contactForm';
+import WinSquad from './Components/squadPictures';
 
 function App() {
   return (
-    <div>
-      <LandingPage />{' '}
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+          <Route path="/about">
+            <WinSquad />
+            <ContactForm />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
