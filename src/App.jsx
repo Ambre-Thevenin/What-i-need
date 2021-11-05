f2729ecdd5fd660c32756cc273e27b07bfd31619
Mel <melissa.olas@gmail.com>
2021-11-05 12:08:38 +0100

    pres test

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
-// import LandingPage from './Components/LandingPage';
+import LandingPage from './Components/LandingPage';
 import './App.css';
 import ContactForm from './components/contactForm';
-import Nav from './components/Nav';
+import Header from './components/Header';
 import WinSquad from './components/squadPictures';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Nav />
-      <WinSquad />
-      <ContactForm />
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <Header />
+            <WinSquad />
+            <ContactForm />
+          </Route>
+          <Route path="/Components/LandingPage" component={LandingPage}>
+            <LandingPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
