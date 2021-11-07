de8a88ee6afafade940d14511232faff8c69d3ec
Mel <melissa.olas@gmail.com>
2021-11-07 20:32:09 +0100

    useless assets deleted and one more route added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import LandingPage from './Components/LandingPage';
 import './App.css';
+import ContactForm from './components/contactForm';
+// import Nav from './components/Nav';
+import WinSquad from './components/squadPictures';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <LandingPage />{' '}
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/Components/LandingPage" component={LandingPage}>
+              <LandingPage />
+            </Route>
+            <Route path="/Components/LandingPage/About" component={(WinSquad, ContactForm)}>
+              <WinSquad />
+              <ContactForm />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
