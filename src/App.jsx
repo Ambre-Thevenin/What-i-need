b19649915e61319637f20e857cf9723b396c52d8
Ambre-Thevenin <ambre.thvn@gmail.com>
2021-11-07 15:53:55 +0100

    creation of routes - fix and tests of non working scripts

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import LandingPage from '../Components/LandingPage';
+import { Route, Switch } from 'react-router-dom';
+import LandingPage from './Components/LandingPage';
 import './App.css';
 import ContactForm from './Components/contactForm';
 import WinSquad from './Components/squadPictures';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route exact path="/">
-            <LandingPage />
-          </Route>
-          <Route path="/about">
-            <WinSquad />
-            <ContactForm />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <div>
+      <Switch>
+        <Route exact path="/">
+          <LandingPage />
+        </Route>
+        <Route path="/about">
+          <WinSquad />
+          <ContactForm />
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
