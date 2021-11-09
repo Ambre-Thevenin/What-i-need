e248a5d7848f56c697da4ea60aa60c1cb1c06fe6
Matthieu Lemarchand <lemarchand.matth@gmail.com>
2021-11-09 17:24:40 +0100

    fixed css for App

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import LandingPage from './components/LandingPage';
 import { Route, Switch } from 'react-router-dom';
 import ContactForm from './components/contactForm';
 import WinSquad from './components/squadPictures';
+
+import './App.css';
+
 // import MeteoForecast from './Components/MeteoForecast';
 
 function App() {
