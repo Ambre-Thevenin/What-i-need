5851fc72b27051ee3e98c09601b0cd6cb2fdf5a4
Ambre-Thevenin <ambre.thvn@gmail.com>
2021-11-09 16:29:34 +0100

    fix : fix typo on eslint ; fix react-router-dom version on package.json to resolve routes issues ; fix App.jsx with route creation ; state leverage for address data

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,20 @@ import './App.css';
 import ContactForm from './components/contactForm';
 // import Nav from './components/Nav';
 import WinSquad from './components/squadPictures';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+// import MeteoForecast from './Components/MeteoForecast';
 
 function App() {
   return (
     <div>
-      <Router>
-        <div>
-          <Switch>
-            <Route exact path="/" component={LandingPage}>
-              <LandingPage />
-            </Route>
-            <Route path="/About" component={(WinSquad, ContactForm)}>
-              <WinSquad />
-              <ContactForm />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
+      <Switch>
+        <Route exact path="/" component={LandingPage}></Route>
+        <Route path="/About">
+          <WinSquad />
+          <ContactForm />
+        </Route>
+      </Switch>
+      {/* <MeteoForecast /> */}
     </div>
   );
 }
--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import CurrentDate from './Date';
 import StartAstroPage from './ModuleAstro';
 import StartMeteoPage from './ModuleMeteo';
@@ -11,6 +10,23 @@ import Nav from '../components/Nav';
 import styles from './LandingPage.module.css';
 
 function LandingPage() {
+  const [destinationData, setDestinationData] = useState();
+  const [originData, setOriginData] = useState();
+
+  function handleOrigin(adress) {
+    setOriginData(adress);
+  }
+
+  function handleDestination(adress) {
+    setDestinationData(adress);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    localStorage.setItem('origin', originData);
+    localStorage.setItem('destination', destinationData);
+  };
+
   return (
     <main className={styles.main}>
       <header className={styles.header}>
@@ -22,7 +38,7 @@ function LandingPage() {
         <CurrentDate />
       </div>
       <div className={styles.input}>
-        <AdressSearch />
+        <AdressSearch origin={handleOrigin} destination={handleDestination} handleSubmit={handleSubmit} />
       </div>
 
       <div className={styles.cards}>
