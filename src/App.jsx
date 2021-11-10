8f6d42177fdcc75c3bde998a0377f74e1a78b232
Mel <melissa.olas@gmail.com>
2021-11-10 14:18:36 +0100

    packages update

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,22 @@ import './App.css';
 import ContactForm from './components/contactForm';
 // import Nav from './components/Nav';
 import WinSquad from './components/squadPictures';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Router>
-        <div>
-          <Switch>
-            <Route exact path="/" component={LandingPage}>
-              <LandingPage />
-            </Route>
-            <Route path="/About" component={(WinSquad, ContactForm)}>
-              <WinSquad />
-              <ContactForm />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+          <Route path="/About">
+            <WinSquad />
+            <ContactForm />
+          </Route>
+        </Switch>
+      </div>
     </div>
   );
 }
