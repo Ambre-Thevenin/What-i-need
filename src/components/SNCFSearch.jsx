608a454eca5e94558317268e0d4fcd6ddb2f9b26
Matthieu Lemarchand <lemarchand.matth@gmail.com>
2021-11-13 21:10:31 +0100

    Modified tokenkey with .env

--- src/components/SNCFSearch.jsx
+++ src/components/SNCFSearch.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import Journey from './Journey';
+require('dotenv').config();
 
 //const api_SNCF = '33c6537e-59dc-429c-af63-ce45208739c6';
 
@@ -10,11 +11,9 @@ function SNCFSearch() {
   const getJourney = () => {
     axios
       .get(
-        'https://api.navitia.io/v1/coverage/sncf/journeys?from=stop_area%3ASNCF%3A87986505&to=stop_area%3ASNCF%3A87382499&key=33c6537e-59dc-429c-af63-ce45208739c6',
+        `https://api.navitia.io/v1/coverage/sncf/journeys?from=stop_area%3ASNCF%3A87986505&to=stop_area%3ASNCF%3A87382499&key=${process.env.TOKENKEY_SNCF}`,
       )
       .then((res) => setSNCFfind(res.data.journeys[0]));
-    // eslint-disable-next-line no-console
-    console.log(SNCFfind);
   };
   return (
     <div>
