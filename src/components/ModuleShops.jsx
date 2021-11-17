3395a77ee38601bfd9891bf0a50df998917a76b8
Matthieu Lemarchand <lemarchand.matth@gmail.com>
2021-11-17 14:46:28 +0100

    Same CSS and architecture for all modules - here shops and sncf aligned to others

--- src/components/ModuleShops.jsx
+++ src/components/ModuleShops.jsx
@@ -2,28 +2,23 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-to-interactive-role */
 import React, { useState } from 'react';
 import ShopsSearch from './ShopsSearch';
+import shopping_logo from './assets/shopping_logo.png';
 
 import styles from './LandingPage.module.css';
 
 function StartShopsPage({ arrivalLatitude, arrivalLongitude }) {
-  const [shopsPage, setShopsPage] = useState('isHidden');
+  const [shopsPage, setShopsPage] = useState('collapsed', 'opened');
 
-  if (shopsPage === 'isHidden') {
+  if (shopsPage === 'collapsed') {
     return (
       <div className={styles.card}>
-        <img
-          src="https://www.radars-auto.com/carte-radar/carte-france-index.png"
-          role="button"
-          alt="cardShops"
-          className={styles.imgCard}
-          onClick={() => setShopsPage('!isHidden')}
-        />
+        <img src={shopping_logo} alt="Shops icon" role="button" className={styles.sparkleCard} onClick={() => setShopsPage('opened')} />
       </div>
     );
   } else {
     return (
       <div className={styles.mainCard}>
-        <button onClick={() => setShopsPage('isHidden')}>Exit</button>
+        <button onClick={() => setShopsPage('collapsed')}>Exit</button>
         <ShopsSearch arrivalLatitude={arrivalLatitude} arrivalLongitude={arrivalLongitude} />
       </div>
     );
