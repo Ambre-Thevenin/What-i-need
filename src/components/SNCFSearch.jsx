2be940022e182a0e10a082b5ff542a97786d8fa2
Matthieu Lemarchand <lemarchand.matth@gmail.com>
2021-11-18 14:41:59 +0100

    remove comments

--- src/components/SNCFSearch.jsx
+++ src/components/SNCFSearch.jsx
@@ -9,7 +9,6 @@ function SNCFSearch({ departureLatitude, departureLongitude, arrivalLatitude, ar
   const getJourney = () => {
     axios
       .get(
-        //`https://api.navitia.io/v1/coverage/sncf/journeys?from=stop_area%3ASNCF%3A87986505&to=stop_area%3ASNCF%3A87382499&key=${process.env.TOKENKEY_SNCF}`,
         `https://api.navitia.io/v1/coverage/sncf/journeys?from=${departureLongitude}%3B${departureLatitude}&to=${arrivalLongitude}%3B${arrivalLatitude}&max_duration_to_pt=50000&key=33c6537e-59dc-429c-af63-ce45208739c6`,
       )
 
--- src/components/ShopsSearch.jsx
+++ src/components/ShopsSearch.jsx
@@ -9,7 +9,6 @@ function ShopsSearch({ arrivalLatitude, arrivalLongitude }) {
     axios
       .get(
         `https://api.navitia.io/v1/coverage/fr-idf/addresses/${arrivalLongitude}%3B${arrivalLatitude}/poi_types/poi_type%3Ashop%3Asupermarket/pois?distance=1000&key=c4978a47-6678-4403-bb27-363d03d40865`,
-        //'https://api.navitia.io/v1/coverage/fr-idf/addresses/2.36764%3B48.85319/poi_types/poi_type%3Ashop%3Aclothes/pois?distance=300&key=c4978a47-6678-4403-bb27-363d03d40865',
       )
       .then((res) => setShopsfind(res.data.pois));
   };
