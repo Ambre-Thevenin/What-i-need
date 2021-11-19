2bc704119b391ef50e0a9f5f26cfcffd9e493189
Ambre-Thevenin <ambre.thvn@gmail.com>
2021-11-19 13:53:12 +0100

    moved index.jsx

---
+++ index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import App from './src/App';
+
+ReactDOM.render(
+  <Router basename={process.env.PUBLIC_URL}>
+    <App />
+  </Router>,
+  document.querySelector('#root'),
+);
